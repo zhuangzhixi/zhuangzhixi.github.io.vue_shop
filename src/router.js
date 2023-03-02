8252d5117eda71b9c40a71d85d5c44fc83b42607
zhuangzhixi <2018418308@qq.com>
2023-03-02 10:09:19 +0800

    完成了商品的开发

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '@/components/login.vue'
+import Login from './components/Login.vue'
 import Home from './components/Home.vue'
 import Welcome from './components/Welcome.vue'
 import Users from './components/user/Users.vue'
@@ -8,6 +8,10 @@ import Rights from './components/power/Rights.vue'
 import Roles from './components/power/Roles.vue'
 import Cate from './components/goods/Cate.vue'
 import Params from './components/goods/Params.vue'
+import GoodsList from './components/goods/List.vue'
+import Add from './components/goods/Add.vue'
+// import Order from './components/order/Order.vue'
+// import Report from './components/report/Report.vue'
 //可以用@代表src
 
 Vue.use(Router)
@@ -27,7 +31,11 @@ const router = new Router({
         { path: '/rights', component: Rights },
         { path: '/roles', component: Roles },
         { path: '/categories', component: Cate },
-        { path: '/params', component: Params }
+        { path: '/params', component: Params },
+        { path: '/goods', component: GoodsList },
+        { path: '/goods/add', component: Add },
+        // { path: '/orders', component: Order },
+        // { path: '/reports', component: Report }
       ]
     }
   ]
