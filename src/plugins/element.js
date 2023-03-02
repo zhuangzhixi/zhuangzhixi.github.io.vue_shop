1afe641ef0df7ca6a4c3126325fe6f88b15df323
zhuangzhixi <2018418308@qq.com>
2023-03-02 16:07:47 +0800

    完成了订单功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -26,8 +26,16 @@ import {
     Cascader,
     Alert,
     Tabs,
-    TabPane
+    TabPane,
+    Steps,
+    Step,
+    CheckboxGroup,
+    Checkbox,
+    Upload
 } from 'element-ui'
+
+import Timeline from './timeline/index.js'
+import TimelineItem from './timeline-item/index.js'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -60,6 +68,13 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
+Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 //与上面的不同，上面的是通过el-container这样的标签名进行调用的
 //message要进行全局挂载$message是一个自定义属性，名字不作要求。这样所以组件都可以通过this来访问message
 Vue.prototype.$message = Message
--- src/router.js
+++ src/router.js
@@ -10,8 +10,8 @@ import Cate from './components/goods/Cate.vue'
 import Params from './components/goods/Params.vue'
 import GoodsList from './components/goods/List.vue'
 import Add from './components/goods/Add.vue'
-// import Order from './components/order/Order.vue'
-// import Report from './components/report/Report.vue'
+import Order from './components/order/Order.vue'
+import Report from './components/report/Report.vue'
 //可以用@代表src
 
 Vue.use(Router)
@@ -34,8 +34,8 @@ const router = new Router({
         { path: '/params', component: Params },
         { path: '/goods', component: GoodsList },
         { path: '/goods/add', component: Add },
-        // { path: '/orders', component: Order },
-        // { path: '/reports', component: Report }
+        { path: '/orders', component: Order },
+        { path: '/reports', component: Report }
       ]
     }
   ]
